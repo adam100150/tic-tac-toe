e4583f716fe984f90cb94eff782bb054e080a0dd
Adam Ashkenazi <adam100150@gmail.com>
2021-12-29 19:01:33 -0800

    Changing the game from PvP to playing against the computer

--- game_funcs.py
+++ game_funcs.py
@@ -36,3 +36,35 @@ def update_gameboard(gameboard, pos, current_player):
          gameboard[2][1] = current_player
     elif pos[0] > 325 and pos[1] > 300: #bottom right box
          gameboard[2][2] = current_player
+
+def draw_at_grid_cord(screen, cord):
+    print("Drawing square")
+    if cord == (0,0): #top left box
+        pg.draw.rect(screen, (0,0,255), (75,75,50,50))
+    elif cord == (1,0): # middle left box
+        pg.draw.rect(screen, (0,0,255), (50,200,50,50))
+    elif cord == (2,0): #bottom left box
+        pg.draw.rect(screen, (0,0,255), (50,350,50,50))
+    elif cord == (0,1): #top middle box
+        pg.draw.rect(screen, (0,0,255), (225,50,50,50))
+    elif cord == (1,1): # middle centered box
+         pg.draw.rect(screen, (0,0,255), (225,200,50,50))
+    elif cord == (2,1): #bottom middle box
+         pg.draw.rect(screen, (0,0,255), (50,50,50,50))
+    elif cord == (0,2): #top right box
+         pg.draw.rect(screen, (0,0,255), (400,50,50,50))
+    elif cord == (1,2): #middle right box
+         pg.draw.rect(screen, (0,0,255), (400,225,50,50))
+    elif cord == (2,2): #bottom right box
+         pg.draw.rect(screen, (0,0,255), (375,350,50,50))
+
+def check_coord(gameboard, cord):
+    return isinstance(gameboard[cord[0]][cord[1]],str)
+
+def check_for_filled_board(gameboard):
+    filled = True
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            if isinstance(gameboard[i][j],str):
+                filled = False
+    return filled
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # Import and initialize the pg library
 import pygame as pg
 from game_funcs import *
+from random import randrange
+from random import seed
 
+PLAYER = 0
+COMPUTER = 1
 
 pg.init()
 
@@ -22,9 +26,6 @@ pg.draw.line(screen, (0, 0, 255), (50,150), (450,150))
 pg.draw.line(screen, (0, 0, 255), (175,50), (175,450))
 pg.draw.line(screen, (0, 0, 255), (325,50), (325,450))
 
-current_player = 0
-counter = 0
-
 # Run until the user asks to quit
 running = True
 while running:
@@ -35,30 +36,48 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
+    #player places a cirlcle
     pos = pg.mouse.get_pos()
     if pg.mouse.get_pressed() == (1,0,0):
-        if current_player == 0:
-            pg.draw.circle(screen, (0, 0, 255), pos, 25)
-            update_gameboard(gameboard, pos, current_player)
-        else:
-            pg.draw.rect(screen, (0, 0, 255), (pos[0], pos[1], 50, 50))
-            update_gameboard(gameboard, pos, current_player)
-        pg.time.delay(150)
+        pg.draw.circle(screen, (0, 0, 255), pos, 25)
+        update_gameboard(gameboard, pos, PLAYER)
+        pg.time.delay(50)
 
-        winner = check_winner(gameboard, current_player)
-        if winner:
-            print(f"The winner is player {current_player}")
+        #computer places a square
+        counter = 0
+        rand_row = randrange(3)
+        rand_col = randrange(3)
+        while not check_coord(gameboard, (rand_row, rand_col)) and counter <= 9:
+            rand_row = randrange(3)
+            rand_col = randrange(3)
+            counter += 1
+        print("Computer coord", rand_row, rand_col)
+
+        gameboard[rand_row][rand_col] = COMPUTER
+        draw_at_grid_cord(screen, (rand_row, rand_col))
+
+        player_win = check_winner(gameboard, PLAYER)
+        computer_win = check_winner(gameboard, COMPUTER)
+
+        winner = None
+        if player_win:
+            winner = PLAYER
+        elif computer_win:
+            winner = COMPUTER
+
+        if winner is not None:
+            print(f"The winner is {winner}")
             screen.fill((255, 255, 255))
             pg.time.delay(75)
-            text_surface = pg.font.Font.render(pg.font.SysFont("arial", 30), f"The winner is player {current_player}", True, (0,0,255))
+            text_surface = pg.font.Font.render(pg.font.SysFont("arial", 30), f"The winner is {winner}", True, (0,0,255))
             pg.Surface.blit(screen, text_surface, (100,100))
 
-        counter += 1
-        current_player = counter % 2
         pg.time.delay(75)
 
 
-    #print(gameboard)
+    print(gameboard)
+    if check_for_filled_board(gameboard):
+        pg.quit()
 
 
     # Flip the display
